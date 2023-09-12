564f5a6ec24dd5fcefb58de189904f11a0dd0956
otto <ottofy@zohomail.com>
2023-09-12 11:59:01 -0600

    update scripts

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,9 @@
 'use client';
 
 import { Post } from 'contentlayer/generated';
-// import Image from 'next/image';
 import Link from 'next/link';
 
-import { CircleIcon, Logo } from '../../svg/icons';
+import { Logo } from '../../svg/icons';
 import ThemeToggle from '../Button/theme-toggle';
 
 import styles from './Header.module.css';
@@ -13,34 +12,39 @@ interface HeaderInterface {
   activePosts: Post[];
 }
 
-const Header: React.FC<HeaderInterface> = ({ activePosts }) => (
-  <header className={styles.header}>
-    <div className={styles.wrapper}>
-      <div className={styles.colLeft}>
-        <Link className={styles.appname} href='/'>
-          BLOG
-        </Link>
-      </div>
-      <div className={styles.colCenter}>
-        <span>
-          <Logo />
-        </span>
-      </div>
-
-      <div className={styles.colRight}>
-        <nav className={styles.nav}>
+const Header: React.FC<HeaderInterface> = ({ activePosts }) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <div className={styles.colLeft}>
           <Link
-            href={
-              `/posts/${activePosts[0].slugAsParams}`
-            }
+            className={styles.appname}
+            href='/'
           >
-            latest
+            BLOG
           </Link>
-        </nav>
+        </div>
+        <div className={styles.colCenter}>
+          <span>
+            <Logo />
+          </span>
+        </div>
+
+        <div className={styles.colRight}>
+          <nav className={styles.nav}>
+            <Link
+              href={
+                `/posts/${activePosts[0].slugAsParams}`
+              }
+            >
+              latest
+            </Link>
+          </nav>
           <ThemeToggle />
+        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
