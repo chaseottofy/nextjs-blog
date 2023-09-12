e005a72b3e2230cf6be2e327ac9f4db1016c82dd
otto <ottofy@zohomail.com>
2023-09-12 16:50:25 -0600

    update paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,79 @@ import '@/styles/mdx.css';
 
 import { Post } from 'contentlayer/generated';
 import React from 'react';
+import localFont from 'next/font/local';
 
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import ThemeProvider from '@/providers/next-theme-provider';
-import { BasementExpanded, NeueMontreal } from '@/styles/fonts';
+// import { BasementExpanded, NeueMontreal } from '@/styles/fonts';
 import getPostsSorted from '@/utils/posts/get-posts-sorted';
 
 import styles from './layout.module.css';
 
 export { BASE_META_DATA as metadata } from '@/data/constants';
+
+const NeueMontreal = localFont({
+  src: [
+    {
+      path: './NeueMontreal-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './NeueMontreal-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './NeueMontreal-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  preload: false,
+  variable: '--neue',
+  fallback: [
+    'apple-system',
+    'blinkmacsystemfont',
+    'segoe ui',
+    'roboto',
+    'oxygen',
+    'ubuntu',
+    'cantarell',
+    'fira sans',
+    'droid sans',
+    'helvetica neue',
+    'sans-serif',
+  ],
+});
+
+const BasementExpanded = localFont({
+  src: [
+    {
+      path: './basement-expanded.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  display: 'block', // No other font matches its weird height, so block it
+  variable: '--basement-expanded',
+  fallback: [
+    'apple-system',
+    'blinkmacsystemfont',
+    'segoe ui',
+    'roboto',
+    'oxygen',
+    'ubuntu',
+    'cantarell',
+    'fira sans',
+    'droid sans',
+    'helvetica neue',
+    'sans-serif',
+  ],
+  preload: false,
+});
 
 interface RootLayoutInterface {
   children: React.ReactNode;
